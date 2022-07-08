f0f7716b9ba00f0dfb3228e99870e4910341fb6c
rHistel <r.kasper@mission-webstyle.de>
2022-07-08 14:17:38 +0200

    TestCommit

---
+++ src/de/rhistel/TestTestTest.java
@@ -0,0 +1,22 @@
+package de.rhistel;
+
+/**
+ * Kurzbeschreibung
+ */
+public class TestTestTest {
+	
+	//region 0. Konstanten
+	//endregion
+	
+	//region 1. Decl and Init Attribute
+	//endregion
+	
+	//region 2. Konstruktoren
+	//endregion
+	
+	//region 3. Getter und Setter
+	//endregion
+	
+	//region 4. Methoden und Funktionen
+	//endregion
+}
